11a18c100ac18e1a896478f48484d48227e63492
sami purmonen <purmonen@kth.se>
2015-05-07 23:54:48 +0200

    Fixed bug

--- Rosa/TemperatureView.swift
+++ Rosa/TemperatureView.swift
@@ -1,15 +1,7 @@
-//
-//  TemperatureView.swift
-//  Rosa
-//
-//  Created by Sami Purmonen on 07/05/15.
-//  Copyright (c) 2015 Sami Purmonen. All rights reserved.
-//
-
 import UIKit
 
 class TemperatureView: UIView {
-    let temperatures = (0..<15).map { _ in 10 + Int(rand()) % 20 }
+    let temperatures = (0..<30).map { _ in 10 + Int(rand()) % 20 }
     
     override func drawRect(rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
@@ -25,14 +17,13 @@ class TemperatureView: UIView {
         
         // Draw temperatures
         CGContextSetStrokeColorWithColor(context, UIColor.blueColor().CGColor)
-        CGContextMoveToPoint(context, 0, CGFloat(temperatures[0]))
         for (i, temperature) in enumerate(temperatures) {
             if i == 0 {
-                continue
+                CGContextMoveToPoint(context, 0, CGFloat(bounds.height) - CGFloat(temperature))
+            } else {
+                CGContextAddLineToPoint(context, CGFloat(i*Int(bounds.width)/(temperatures.count-1)), CGFloat(bounds.height) - CGFloat(temperature))
             }
-            CGContextAddLineToPoint(context, CGFloat(i*Int(bounds.width)/(temperatures.count-1)), CGFloat(temperature)*4)
         }
-
         CGContextStrokePath(context)
     }
     
