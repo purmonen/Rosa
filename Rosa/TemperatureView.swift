0230d9fbf1d947ab29f724f79f34bb63c6b6f078
sami purmonen <purmonen@kth.se>
2015-05-07 23:17:28 +0200

    Added temperature axis

--- Rosa/TemperatureView.swift
+++ Rosa/TemperatureView.swift
@@ -9,24 +9,27 @@
 import UIKit
 
 class TemperatureView: UIView {
-
-    //1 - the properties for the gradient
-    @IBInspectable var startColor: UIColor = UIColor.redColor()
-    @IBInspectable var endColor: UIColor = UIColor.greenColor()
-    
-    
-//    let temperatures = [10,15,12,24,21,19,27,21,15,14,13]
     let temperatures = (0..<15).map { _ in 10 + Int(rand()) % 20 }
     
     override func drawRect(rect: CGRect) {
-        
-        //2 - get the current context
         let context = UIGraphicsGetCurrentContext()
-        CGContextSetStrokeColorWithColor(context, UIColor.blueColor().CGColor)
         CGContextSetLineWidth(context, 2.0)
-        CGContextMoveToPoint(context, 0, 0)
+        CGContextSetStrokeColorWithColor(context, UIColor.lightGrayColor().CGColor)
         
+        // Draw axis
+        CGContextMoveToPoint(context, 0, 0)
+        CGContextAddLineToPoint(context, 0, bounds.height)
+        CGContextAddLineToPoint(context, bounds.width, bounds.height)
+        CGContextStrokePath(context)
+        
+        
+        // Draw temperatures
+        CGContextSetStrokeColorWithColor(context, UIColor.blueColor().CGColor)
+        CGContextMoveToPoint(context, 0, CGFloat(temperatures[0]))
         for (i, temperature) in enumerate(temperatures) {
+            if i == 0 {
+                continue
+            }
             CGContextAddLineToPoint(context, CGFloat(i*Int(bounds.width)/(temperatures.count-1)), CGFloat(temperature)*4)
         }
 
