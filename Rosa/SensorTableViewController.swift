abd28caefe746bc121887fa29a6afc1961b0e784
Paul Griffin <paul.griffin@hotmail.com>
2015-05-07 21:44:56 +0200

    Added mock data

--- Rosa/SensorTableViewController.swift
+++ Rosa/SensorTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class SensorTableViewController: UITableViewController {
 
     let sensors = [
@@ -91,14 +92,12 @@ class SensorTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if let indexPath = tableView.indexPathForSelectedRow(),
+            let dv = segue.destinationViewController as? SensorViewController {
+            dv.sensor = sensors[indexPath.row]
+        }
     }
-    */
 
 }
